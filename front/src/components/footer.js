98487453616978da6a171e75a111082991099b4a
FEJTWOW <kamil.wierciak99@gmail.com>
2020-08-29 15:22:26 +0200

    add footer

--- front/src/components/footer.js
+++ front/src/components/footer.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Header = styled.header`
+
+const Footer = styled.footer`
     height: 4em;
     background-color: #e2e2e2;
 `;
 
 
 export default () => {
-
     return(
-        <Header>
-            <div>dwdwd</div>
-            <div>dwdwdwdwdwdw</div>
-        </Header>
+        <Footer>
+            <p>Author: ***Coders</p>
+        </Footer>
     );
 }
