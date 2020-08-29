6840d33cfe8b66528d10bb0beb0c5891e8c51663
Meqeq <mekekwengiel25@gmail.com>
2020-08-29 15:53:23 +0200

    navigation

--- front/src/components/header.js
+++ front/src/components/header.js
@@ -1,21 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    NavbarText
+} from 'reactstrap';
 
 import {Link} from 'react-router-dom';
 
 const Header = styled.header`
-    height: 4em;
+    min-height: 4em;
     background-color: #e2e2e2;
+
+    a {
+        color: #000;
+    }
 `;
 
 
 export default () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
 
     return(
         <Header>
-            <div>dwdwdwdwdwdw</div>
-            <Link to="/">Home</Link>
-            <Link to="/animal">Zwierze</Link>
+            <Navbar light expand="md">
+                <NavbarBrand href="/">Nazwa_naszej_aplikacji</NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                <Nav className="mr-auto" navbar>
+                    <NavItem>
+                        <Link className="nav-link" to="/">Home</Link>
+                    </NavItem>
+                    <NavItem>
+                        <Link className="nav-link" to="/animal">Zwierze</Link>
+                    </NavItem>
+                    <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                        Options
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                        <DropdownItem>
+                        Option 1
+                        </DropdownItem>
+                        <DropdownItem>
+                        Option 2
+                        </DropdownItem>
+                        <DropdownItem divider />
+                        <DropdownItem>
+                        Reset
+                        </DropdownItem>
+                    </DropdownMenu>
+                    </UncontrolledDropdown>
+                </Nav>
+                <NavbarText>Podejmij wyzwanie</NavbarText>
+                </Collapse>
+            </Navbar>
         </Header>
     );
 }
