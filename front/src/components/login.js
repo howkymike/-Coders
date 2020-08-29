eca95bfa2a7cd1d47f7c8e5edfaa13b9a302c0b2
Meqeq <mekekwengiel25@gmail.com>
2020-08-30 00:00:16 +0200

    login/register box added

---
+++ front/src/components/login.js
@@ -0,0 +1,74 @@
+import React, { useState, useContext } from 'react';
+import styled from 'styled-components';
+import { Container, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+
+import { UserContext } from '../context/userContext';
+
+const LoginBox = styled.div` 
+    width: 30em;
+    background-color: #e2e2e2;
+    margin-left: 30%;
+    box-shadow: 0 4px 5px 0 rgba(0, 0, 0, .14), 0 1px 10px 0 rgba(0, 0, 0, .12), 0 2px 4px -1px rgba(0, 0, 0, .20);
+    border-radius: 5px;
+    padding: 1em;
+
+    @media (max-width: 768px) {
+        margin: auto;
+    }
+`;
+
+const ButtonWrapper = styled.div` 
+    display: flex;
+    justify-content: space-between;
+
+    button {
+        width: 45%;
+    }
+`;
+
+export default () => {
+    let [type, setType] = useState(true);
+    let [form, setForm] = useState({ login: "", pass: "", pass2: "" });
+    let { login, register } = useContext(UserContext);
+
+    const formSubmit = e => {
+        e.preventDefault();
+        if(type)
+            login(form.login, form.pass);
+        else
+            register(form.login, form.pass, form.pass2);
+    }
+
+    return(
+        <LoginBox onSubmit={ e => formSubmit(e) }>
+            { type ? "Zaloguj się!" : "Zarejestruj się!" }
+            <hr />
+            <Form>
+                <FormGroup>
+                    <Label for="name">Login</Label>
+                    <Input type="text" id="name" placeholder="..." value={form.login} onChange={ e => setForm({...form, login: e.target.value})} />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="pass">Password</Label>
+                    <Input type="password" id="pass" placeholder="..." value={form.pass} onChange={ e => setForm({...form, pass: e.target.value})} />
+                </FormGroup>
+                { !type &&
+                    <FormGroup>
+                        <Label for="pass">Password</Label>
+                        <Input type="password" id="pass" placeholder="..." value={form.pass2} onChange={ e => setForm({...form, pass2: e.target.value})} />
+                    </FormGroup>
+                }
+                { type ?
+                    <ButtonWrapper>
+                        <Button color="success" size="lg">Login</Button>
+                        <Button color="danger" size="lg" onClick={ () => setType(false)}>Register</Button>
+                    </ButtonWrapper> :
+                    <ButtonWrapper>               
+                        <Button color="success" size="lg">Register</Button>
+                        <Button color="danger" size="lg" onClick={ () => setType(true)}>Login</Button>
+                    </ButtonWrapper>
+                }
+            </Form>
+        </LoginBox>
+    );
+}
--- front/src/pages/addChallenge.js
+++ front/src/pages/addChallenge.js
@@ -35,42 +35,42 @@ export default () => {
                     <hr />
                 </Header>
                 <Form>
-                <FormGroup>
-                    <Label for="name">Name</Label>
-                    <Input type="text" id="name" placeholder="..." value={ form.name } onChange={ e => setFormField("name", e.target.value) }/>
-                </FormGroup>
-                <FormGroup>
-                    <Label for="category">Category</Label>
-                    <Input type="select" id="category" value={ form.category } onChange={ e => setFormField("category", e.target.value) }>
-                        <option value="" disabled>...</option>
-                        { ["it", "sport", "kek"].map( (value, key) => (
-                            <option key={ key } value={ value }>{ value }</option>
-                        )) }
-                    </Input>
-                </FormGroup>
-                <FormGroup>
-                    <Label for="desc">Description</Label>
-                    <Input type="textarea" id="desc" placeholder="..." value={ form.desc } onChange={ e => setFormField("desc", e.target.value) } />
-                </FormGroup>
-                <hr />
-                <FormGroup>
-                    <Label for="verification">Verification method</Label>
-                    <Input type="select" id="verification" value={ form.verification } onChange={ e => setFormField("verification", e.target.value) }>
-                        <option value="" disabled>...</option>
-                        { [{val: "question", name:"question"}, {val:"mod", name:"approved by moderator"}].map( (value, key) => (
-                            <option key={ key } value={ value.val }>{ value.name }</option>
-                        )) }
-                    </Input>
-                </FormGroup>
-                { form.verification === "question" && 
                     <FormGroup>
-                        <Label for="question">Question</Label>
-                        <Input type="text" id="question" value={ form.question } onChange={ e => setFormField("question", e.target.value) } placeholder="Give a question" />
-                        <Label for="answer">Answear</Label>
-                        <Input type="text" id="answer" value={ form.answer } onChange={ e => setFormField("answer", e.target.value) } placeholder="Correct answear" />
+                        <Label for="name">Name</Label>
+                        <Input type="text" id="name" placeholder="..." value={ form.name } onChange={ e => setFormField("name", e.target.value) }/>
                     </FormGroup>
-                }
-                <Button color="primary" size="lg" block>Submit</Button>
+                    <FormGroup>
+                        <Label for="category">Category</Label>
+                        <Input type="select" id="category" value={ form.category } onChange={ e => setFormField("category", e.target.value) }>
+                            <option value="" disabled>...</option>
+                            { ["it", "sport", "kek"].map( (value, key) => (
+                                <option key={ key } value={ value }>{ value }</option>
+                            )) }
+                        </Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="desc">Description</Label>
+                        <Input type="textarea" id="desc" placeholder="..." value={ form.desc } onChange={ e => setFormField("desc", e.target.value) } />
+                    </FormGroup>
+                    <hr />
+                    <FormGroup>
+                        <Label for="verification">Verification method</Label>
+                        <Input type="select" id="verification" value={ form.verification } onChange={ e => setFormField("verification", e.target.value) }>
+                            <option value="" disabled>...</option>
+                            { [{val: "question", name:"question"}, {val:"mod", name:"approved by moderator"}].map( (value, key) => (
+                                <option key={ key } value={ value.val }>{ value.name }</option>
+                            )) }
+                        </Input>
+                    </FormGroup>
+                    { form.verification === "question" && 
+                        <FormGroup>
+                            <Label for="question">Question</Label>
+                            <Input type="text" id="question" value={ form.question } onChange={ e => setFormField("question", e.target.value) } placeholder="Give a question" />
+                            <Label for="answer">Answear</Label>
+                            <Input type="text" id="answer" value={ form.answer } onChange={ e => setFormField("answer", e.target.value) } placeholder="Correct answear" />
+                        </FormGroup>
+                    }
+                    <Button color="primary" size="lg" block>Submit</Button>
                 </Form>
             </Container>
         </Wrapper>
