2b44cbf7969bea74f5a5aea73da9d6ea5313dbec
Meqeq <mekekwengiel25@gmail.com>
2020-08-29 22:43:04 +0200

    context and global style added

--- front/src/components/globalStyle.js
+++ front/src/components/globalStyle.js
@@ -1,6 +1,10 @@
 import {createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+    html {
+        font-size: calc(9px + 6 * ((100vw - 320px) / 1600));
+    }
+
     body {
         font-family: 'Press Start 2P', cursive;
     }
