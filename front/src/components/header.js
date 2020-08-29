9e6b8a09cc30fe43a867eddd9ea7027b1d4f7517
Meqeq <mekekwengiel25@gmail.com>
2020-08-29 15:07:45 +0200

    reactstrap i styled components

---
+++ front/src/components/header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Header = styled.header`
+    height: 4em;
+    background-color: #e2e2e2;
+`;
+
+
+export default () => {
+
+
+
+    return(
+        <Header>
+            <div>dwdwd</div>
+            <div>dwdwdwdwdwdw</div>
+        </Header>
+    );
+}
