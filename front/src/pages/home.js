a0b787d07023299d4235212497ddaf046d2f00cd
FEJTWOW <kamil.wierciak99@gmail.com>
2020-08-29 15:52:17 +0200

    add responsive background image

--- front/src/pages/home.js
+++ front/src/pages/home.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const Background = styled.div`
+    background-image: url("/background.jpg");
+    height: 100%; 
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+`;
 
 export default () => {
 
     return(
-        <div>HOME</div>
+        <Background></Background>
     );
 }
